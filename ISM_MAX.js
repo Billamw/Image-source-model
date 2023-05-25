a09c2060f626e97ac721f95213e42f5419e99159
Billamw <johan.dasbach@gmx.de>
2023-05-25 09:48:38 +0200

    Update ISM_MAX.js

--- ISM_MAX.js
+++ ISM_MAX.js
@@ -1,59 +1,12 @@
 const math = require('mathjs');
 
-// Corners will be inserted in order: Top left, Top right, Bottom right, Bottom left
-const corners = [[0,10,0], [10,10,0], [10,0,0], [0,0,0], [0,10,10], [10,10,10], [10,0,10], [0,0,10],[0,10,20], [10,10,20], [10,0,20], [0,0,20]];
-
-const speaker = [5,5,3];
-const microfon = [5,5,4];
-
-const Walls = gernerateWalls(corners);
-
-//////////////
-// OutPut();//
-//////////////
-
-function gernerateWalls(corners = []) {
-    if(corners.length % 4 != 0) {
-        throw {name: "Invalid room geometry", message:"Number of corners must be divisable by 4( Corners: " + corners.length + ")"};
-    }
-
-    const backWall = [corners[0], corners[1], corners[2], corners[3]];
-    const frontWall = [corners[corners.length-4], corners[corners.length-3], corners[corners.length-2], corners[corners.length-1]];
-
-    const countOfRings = corners.length/4;
-    let walls = [];
-    let wall = [];
-
-    walls[0] = backWall;
-    for(let i = 0; i < (countOfRings - 1) * 4; i++) {
-        // declaration of the following corners in the same directions
-
-        // Floor (Corners are added counter clock wise(watched from outside))
-        if(i % 4 == 2) {
-            wall = [corners[i], corners[i+4], corners[i+5], corners[i+1]];
-        }
-        // left Wall
-        else if(i % 4 == 3) {
-            wall = [corners[i], corners[i+4], corners[i+1], corners[i-3]];
-        }
-        // Ceiling and Floor
-        else {
-            wall = [corners[i], corners[i+1], corners[i+5], corners[i+4]];
-        }
-        walls[i+1] = wall;
-    }
-    walls[walls.length] = frontWall;
-    
-    return walls;
-}
-
 //lvec: location vector ist the first vector of each wall
 //svec: support vector wall[3]-wall[0]
 //dvec: direction vector wall[1]-wall[0]
-function getImageSoundSource(wall = [], speaker = []) {
-    let lvec = wall[0];
-    let svec = math.subtract(wall[1], wall[0]);
-    let dvec = math.subtract(wall[3], wall[0]);
+function getImageSoundSource(polygon = [], speaker = []) {
+    let lvec = polygon[0];
+    let svec = math.subtract(polygon[0], polygon[parseInt((polygon.length-1) / 2)]);
+    let dvec = math.subtract(polygon[0], polygon[polygon.length - parseInt((polygon.length-1) / 2)]);
     let normal = math.cross(dvec, svec);
     normal = math.divide(normal, math.norm(normal));
     let levToSpeaker = math.subtract(lvec, speaker);
@@ -62,27 +15,24 @@ function getImageSoundSource(wall = [], speaker = []) {
     return math.add(speaker, math.multiply(2*lambda, normal));
 }
 
-function calculateIntersection(wall=[], microphone=[], ISS=[]) {
+function calculateIntersection(polygon=[], microphone=[], ISS=[]) {
 
     let lineVector = math.subtract(microphone, ISS);
 
     let planeNormal = math.cross(
-      math.subtract(wall[1], wall[0]),
-      math.subtract(wall[2], wall[0])
+      math.subtract(polygon[0], polygon[parseInt((polygon.length-1) / 2)]),
+      math.subtract(polygon[0], polygon[polygon.length - parseInt((polygon.length-1) / 2)])
     );
   
-    let t = math.dot(planeNormal, math.subtract(wall[0], ISS)) / math.dot(planeNormal, lineVector);
+    let t = math.dot(planeNormal, math.subtract(polygon[0], ISS)) / math.dot(planeNormal, lineVector);
     let intersectionPoint = math.add(ISS, math.multiply(t, lineVector));
   
     return intersectionPoint;
   }
 
-function getDistance(ISS = [], microfon = []) {
-   return math.norm(math.subtract(ISS, microfon));
-}
-
-function containsPoint(point=[], polygon=[]) {
-  let toReturn = false;
+  
+  function containsPoint(point=[], polygon=[]) {
+    let toReturn = false;
   for (let i = 0; i < polygon.length-1; i++) {
     let triangle = [polygon[0], polygon[i], polygon[i+1]];
     const normal = math.cross(math.subtract(triangle[1], triangle[0]), math.subtract(triangle[2], triangle[0]));
@@ -111,43 +61,35 @@ function isInTwoDimSpace(polygon=[]) {
   return true;
 }
 
-// not working method of containsPoint
-  
-  // Beispielanwendung
-  var polygon3D = [[0, 0, 0], [0, 5, 0], [5, 5, 0], [5, 0, 0]];
-  var point3D = [0, 0, 0];
-  // var isInside3D = pointInPolygon3D(point3D, polygon3D);
-  // var isInPolygon1 = isInPolygon(point3D, polygon3D);
-  var isInPolygon = containsPoint(point3D, polygon3D);
-  var isTwoDim = isInTwoDimSpace(polygon3D);
-  // console.log(isInPolygon1)
-  console.log("is inside: " + isInPolygon); // true
-  console.log("is two dim: " + isTwoDim);
-  
+function getDistance(ISS = [], microfon = []) {
+   return math.norm(math.subtract(ISS, microfon));
+}
 
-// function OutPut() {
-//     for(let i = 0; i<Walls.length; i++) {
-//         let distance = getDistance(getImageSoundSource(Walls[i], speaker), microfon);
-//         if(CheckWithBaryzentrical(calculateIntersection(Walls[i], microfon, getImageSoundSource(Walls[i], speaker)), Walls[i])) {
-//             if(i == 0) {
-//                 console.log("back wall.\tReflection distance = " + distance )
-//             }
-//             if(i == Walls.length-1) {
-//                 console.log("front wall.\tReflection distance = " + distance)
-//             }
-//             if(i%4 == 1 && i!= Walls.length-1) {
-//                 console.log((parseInt(i/5)+1) + ". ceiling.\tReflection distance = " + distance)
-//             }
-//             if(i%4 == 2) {
-//                 console.log((parseInt(i/5)+1) + ". right wall.\tReflection distance = " + distance)
-//             }
-//             if(i%4 == 3) {
-//                 console.log((parseInt(i/5)+1) + ". floor.\tReflection distance = " + distance)
-//             }
-//             if(i%4 == 0 && i != 0) {
-//                 console.log((parseInt(i/5)+1) + ". left wall.\tReflection distance = " + distance)
-//             }
-//         }
-//     }
-// }
+
+const polygon = [[0,10,0], [10,10,0], [10,0,0], [0,0,0]];
+
+const speaker = [5,2,4];
+const microfon = [5,6,4];
+
+if(isInTwoDimSpace(polygon)) {
+  const iss = getImageSoundSource(polygon, speaker);
+  const intersection = calculateIntersection(polygon, microfon, iss);
+  const contains = containsPoint(intersection, polygon);
+  console.log("is valid: " + contains)
+} else {
+  console.log("Not in two dim space!")
+}
+
+
+
+// Beispielanwendung
+  // var polygon3D = [[0, 0, 0], [0, 5, 0], [5, 5, 0], [5, 0, 0]];
+  // var point3D = [0, 0, 0];
+  // // var isInside3D = pointInPolygon3D(point3D, polygon3D);
+  // // var isInPolygon1 = isInPolygon(point3D, polygon3D);
+  // var isInPolygon = containsPoint(point3D, polygon3D);
+  // var isTwoDim = isInTwoDimSpace(polygon3D);
+  // // console.log(isInPolygon1)
+  // console.log("is inside: " + isInPolygon); // true
+  // console.log("is two dim: " + isTwoDim);
         
